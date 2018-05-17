b55ef23dbdd9cc68a79e0ec1b879e8e3462caae6
vitallium <vitallium@yandex.ru>
2018-05-17 23:26:40 +0300

    Change UI

--- NeuralNetwork/Form1.Designer.cs
+++ NeuralNetwork/Form1.Designer.cs
@@ -28,71 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageNetwork = new System.Windows.Forms.TabPage();
+            this.splitter1 = new System.Windows.Forms.Splitter();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tabPageParameters = new System.Windows.Forms.TabPage();
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPageNetwork.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPageParameters.SuspendLayout();
             this.SuspendLayout();
             // 
-            // treeView1
+            // menuStrip1
             // 
-            this.treeView1.Location = new System.Drawing.Point(0, 396);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(606, 111);
-            this.treeView1.TabIndex = 0;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.runToolStripMenuItem,
+            this.trainToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(911, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // button1
+            // fileToolStripMenuItem
             // 
-            this.button1.Location = new System.Drawing.Point(612, 396);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Run";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
             // 
-            // button2
+            // openToolStripMenuItem
             // 
-            this.button2.Location = new System.Drawing.Point(612, 466);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Train";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trainToolStripMenuItem
+            // 
+            this.trainToolStripMenuItem.Name = "trainToolStripMenuItem";
+            this.trainToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.trainToolStripMenuItem.Text = "Train";
+            this.trainToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageNetwork);
+            this.tabControl1.Controls.Add(this.tabPageParameters);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(911, 607);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPageNetwork
+            // 
+            this.tabPageNetwork.Controls.Add(this.splitter1);
+            this.tabPageNetwork.Controls.Add(this.pictureBox1);
+            this.tabPageNetwork.Controls.Add(this.treeView1);
+            this.tabPageNetwork.Location = new System.Drawing.Point(4, 22);
+            this.tabPageNetwork.Name = "tabPageNetwork";
+            this.tabPageNetwork.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageNetwork.Size = new System.Drawing.Size(903, 581);
+            this.tabPageNetwork.TabIndex = 0;
+            this.tabPageNetwork.Text = "NeuronNet";
+            this.tabPageNetwork.UseVisualStyleBackColor = true;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(259, 3);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 575);
+            this.splitter1.TabIndex = 6;
+            this.splitter1.TabStop = false;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(259, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(735, 378);
-            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.Size = new System.Drawing.Size(641, 575);
+            this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(256, 575);
+            this.treeView1.TabIndex = 4;
+            // 
+            // tabPageParameters
+            // 
+            this.tabPageParameters.Controls.Add(this.propertyGrid);
+            this.tabPageParameters.Location = new System.Drawing.Point(4, 22);
+            this.tabPageParameters.Name = "tabPageParameters";
+            this.tabPageParameters.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageParameters.Size = new System.Drawing.Size(903, 581);
+            this.tabPageParameters.TabIndex = 1;
+            this.tabPageParameters.Text = "Parameters";
+            this.tabPageParameters.UseVisualStyleBackColor = true;
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.Dock = System.Windows.Forms.DockStyle.Left;
+            this.propertyGrid.Location = new System.Drawing.Point(3, 3);
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.Size = new System.Drawing.Size(322, 575);
+            this.propertyGrid.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(759, 519);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.treeView1);
+            this.ClientSize = new System.Drawing.Size(911, 631);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageNetwork.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPageParameters.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trainToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageNetwork;
+        private System.Windows.Forms.TabPage tabPageParameters;
+        private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PropertyGrid propertyGrid;
     }
 }
 
--- NeuralNetwork/Form1.cs
+++ NeuralNetwork/Form1.cs
@@ -7,17 +7,20 @@ namespace NeuralNet
 {
     public partial class Form1 : Form
     {
-        NeuralNetwork nn = new NeuralNetwork(0.9, new int[] { 3, 3, 3 });
+        NeuralNetwork _neuralNetwork;
 
         public Form1()
         {
             InitializeComponent();
+
+            _neuralNetwork = new NeuralNetwork(0.9, new[] { 3, 3, 3 });
+            propertyGrid.SelectedObject = _neuralNetwork;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            NetworkHelper.ToTreeView(treeView1, nn);
-            NetworkHelper.ToPictureBox(pictureBox1, nn, 400, 100);
+            NetworkHelper.ToTreeView(treeView1, _neuralNetwork);
+            NetworkHelper.ToPictureBox(pictureBox1, _neuralNetwork, 400, 100);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,10 +30,10 @@ namespace NeuralNet
             ins.Add(0.1);
             ins.Add(0.8);
 
-            nn.Run(ins);
+            _neuralNetwork.Run(ins);
 
-            NetworkHelper.ToTreeView(treeView1, nn);
-            NetworkHelper.ToPictureBox(pictureBox1, nn,400, 100);
+            NetworkHelper.ToTreeView(treeView1, _neuralNetwork);
+            NetworkHelper.ToPictureBox(pictureBox1, _neuralNetwork,400, 100);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -46,11 +49,17 @@ namespace NeuralNet
             ots.Add(0.778);
 
             for(int i = 0; i <100000; i++)
-                nn.Train(ins, ots);
+                _neuralNetwork.Train(ins, ots);
 
-            NetworkHelper.ToTreeView(treeView1, nn);
-            NetworkHelper.ToPictureBox(pictureBox1, nn,400, 100);
+            NetworkHelper.ToTreeView(treeView1, _neuralNetwork);
+            NetworkHelper.ToPictureBox(pictureBox1, _neuralNetwork,400, 100);
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
     }
 
 }
